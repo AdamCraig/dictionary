15f69951f25baa4f4ae99f3cd3964c73dc6da15a
Adam Craig <ajcraig@suffolk.edu>
2016-04-29 08:26:17 -0700

    add failing test for Definition objects instantiating with their assigned meaning

--- src/main/java/Definition.java
+++ src/main/java/Definition.java
@@ -1,6 +1,11 @@
 public class Definition {
+  private String mMeaning;
 
   public Definition(String meaning) {
+    mMeaning = meaning;
+  }
 
+  public String getMeaning() {
+    return "";
   }
 }
--- src/test/java/DefinitionTest.java
+++ src/test/java/DefinitionTest.java
@@ -9,4 +9,10 @@ public class DefinitionTest {
     assertEquals(true, myDefinition instanceof Definition);
   }
 
+  @Test
+  public void Definition_instantiatesWithMeaning_String() {
+    Definition myDefinition = new Definition("This is the meaning of this word.");
+    assertEquals("This is the meaning of this word.", myDefinition.getMeaning());
+  }
+
 }
